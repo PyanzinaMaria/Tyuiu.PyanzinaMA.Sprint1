bc7c49cf026116f4f622d25c81f2e1154511d274
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-09-27 19:07:34 +0500

    Task5

---
+++ Tyuiu.PyanzinaMA.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PyanzinaMA.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            
+            int k = 13650964;
+            int h = ds.Calculate(k);
+            int wait = 9;
+            Assert.AreEqual(wait, h);
+            
+
+        }
+    }
+}
---
+++ Tyuiu.PyanzinaMA.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PyanzinaMA.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Пьянзина М. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Пьянзина Мария Алексеевна | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Присвоить целой переменной h третью от конца цифру в записи             *");
+            Console.WriteLine("* положительного целого числа k.                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int k;
+            Console.WriteLine("Введите значение k:");
+            k = Convert.ToInt32(Console.ReadLine());
+            int h = ds.Calculate(k);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"h = {h}");
+
+            Console.ReadLine();
+        }
+    }
+}
